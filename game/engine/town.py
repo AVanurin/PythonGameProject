25eee91c9c4f2229600a217079da5e21deef1ba5
Arkadiy <005ark@gmail.com>
2019-11-07 21:58:52 +0300

    WIP

---
+++ game/engine/town.py
@@ -0,0 +1,33 @@
+
+
+
+class Town(object):
+    def __init__(self, name):
+        self.name = name
+
+my_objects = ['bank','taverna','post']
+
+for i in range(3):
+    my_objects.append(Town(i))
+
+for obj in my_objects:
+    return obj.name
+
+class Bilding:
+    def __init__(self, name, options, description):
+        self.name = name
+        self.description = description
+        self.options = options
+    
+def Generate Bilding():
+    a = []
+
+    b_store = Building("shop", ["sell", "buy"], "[food,clothes",  "хорошие товары"):
+    b_post = Building("post", ["send", "resieve"], "[letter,box]" , "лучшая почта"):
+    b_taverna = Building("taverna", ["food", "drinks", "pay"], "хорошая еда"):
+
+
+     a.append(b_store,b_post, b_taverna)
+
+     return a
+
